1b9079a03d3c5b522deca7ff9b7050dec2d18262
big_richi <big.richi@outlook.com>
2021-02-03 09:50:45 -0500

    commit tree 8f0badddda54d32694f1b33f970c69b4e0df6c76

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+class Dog
+
+    def initialize(breed_att="Mutt")
+        @breed = breed_att
+    end
+
+end
+
+daria = Dog.new("Pitbull")
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+class Person
+
+    def initialize(name_att)
+        @name = name_att
+    end
+    
+end
+
+richard = Person.new("Richard")
